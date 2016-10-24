5f25237ef0df2d812f293b1f00d7e7d20ea4a6dd
Piper Thunstrom <pathunstrom@gmail.com>
2016-10-24 02:52:56 -0400

    Adds tests, fixes Vector bug.

---
+++ js/test/test.js
@@ -0,0 +1,26 @@
+var assert = require('assert');
+
+describe("Vector", function() {
+    var Vector = require('../vector.js');
+    it('should be a function', function () {
+        assert.equal(typeof Vector, 'function');
+    });
+
+    describe('An instance', function () {
+        describe("sub()", function() {
+            it("should return a third vector", function(){
+                var position = new Vector(1, 1);
+                var target = new Vector(3, 3);
+                assert.deepEqual(target.sub(position), new Vector(2, 2))
+            })
+        });
+
+        describe("add()", function() {
+            it("should return a third vector", function() {
+                var position = new Vector(2, 4);
+                var displacement = new Vector(3, 5);
+                assert.deepEqual(position.add(displacement), new Vector(5, 9));
+            })
+        })
+    })
+});
