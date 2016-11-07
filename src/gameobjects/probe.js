8ec00850165d804c09588ce56bc2b001069dd1b5
Piper Thunstrom <pathunstrom@gmail.com>
2016-11-06 23:13:26 -0500

    Adds necessary functions to Probes.

--- src/gameobjects/probe.js
+++ src/gameobjects/probe.js
@@ -8,8 +8,10 @@ function Probe(x, y, space) {
     this.speed = 0.06;
     this.target = null;
     this.color = "rgb(200, 255, 220)"; // TODO: Add parameters.
-    this.behavior = "basicProbe";
+    this.behavior = "collectBeacon";
     this.space = space;
+    this.container = [];
+    this.maxCapacity = 6;
 }
 
 Probe.prototype.draw = function (canvas, x, y) {
@@ -18,6 +20,18 @@ Probe.prototype.draw = function (canvas, x, y) {
     canvas.beginPath();
     canvas.arc(x, y, this.size, 0, Math.PI * 2);
     canvas.stroke();
+};
+
+Probe.prototype.store = function (good) {
+    var capacity = this.container.reduce(function(prev, item){
+        return prev + item.size
+    }, 0);
+    if (capacity + good.size <= this.maxCapacity) {
+        this.container.push(good);
+        return true;
+    } else {
+        return false;
+    }
 };
 
 module.exports = Probe;
